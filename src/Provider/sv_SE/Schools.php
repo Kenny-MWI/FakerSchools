af4e99c76541edd324e6a335743c930d0437e75d
Kenny Johnson <kenny@buildmidwestern.com>
2022-10-04 21:05:25 -0500

    Add sv_SE locale file

---
+++ src/Provider/sv_SE/Schools.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace FakerSchools\Provider\sv_SE;
+
+use Faker\Provider\Base as FakerBase;
+
+class Schools extends FakerBase
+{
+    protected static $universityFormats = [];
+
+    protected static $collegeFormats = [];
+
+    protected static $highSchoolFormats = [];
+
+    protected static $realHighSchools = [];
+
+    protected static $realColleges = [];
+
+    protected static $realUniversities = [];
+
+}
