87e3fcc904d7400919dbaf2a62138b83d1afe18f
Kenny Johnson <kenny@buildmidwestern.com>
2022-10-04 21:54:26 -0500

    Added some real Swedish universities

--- src/Provider/sv_SE/Schools.php
+++ src/Provider/sv_SE/Schools.php
@@ -6,16 +6,84 @@ use Faker\Provider\Base as FakerBase;
 
 class Schools extends FakerBase
 {
-    protected static $universityFormats = [];
+    /**
+     * @var array
+     */
+    protected static $universityFormats = [
+        '{{city}} universitet',
+        '{{city}} Högskola',
+        '{{city}} tekniska universitet'
+    ];
 
+    /**
+     * @todo
+     * @var array
+     */
     protected static $collegeFormats = [];
 
-    protected static $highSchoolFormats = [];
+    /**
+     * @var array
+     */
+    protected static $highSchoolFormats = [
+        '{{city}} Högstadiet',
+        '{{city}} Skola',
+    ];
 
+    /**
+     * @todo
+     * @var array
+     */
     protected static $realHighSchools = [];
 
+    /**
+     * @todo
+     * @var array
+     */
     protected static $realColleges = [];
 
-    protected static $realUniversities = [];
+    /**
+     * @see https://www.rocapply.com/study-in-sweden/sweden-universities/
+     * @var array
+     */
+    protected static $realUniversities = [
+        'Kungliga Tekniska Högskolan',
+        'Stockholms universitet',
+        'Karolinska Institutet',
+        'Högskolan Väst',
+        'Malmö Högskola',
+        'Linnéuniversitetet',
+        'Högskolan Dalarnas',
+        'Högskolan i Borås',
+        'Luleå tekniska universitet',
+        'Högskolan i Skövde',
+        'Göteborgs universitet',
+        'Lunds universitet',
+        'Uppsalas universitet',
+        'Chalmers tekniska högskola',
+        'Umeå universitet',
+        'Örebro universitet',
+        'Högskolan i Jönköping',
+        'Mittuniversitetet',
+    ];
 
+    /**
+     * Ett äkta universitetsnamn.
+     * @return string
+     */
+    public function realUniversity()
+    {
+        return static::randomElement(static::$realUniversities);
+    }
+
+    /**
+     * Ett påhittad universitetsnamn.
+     * @example 'Helsingborg universitet'
+     * @return string
+     */
+    public function university()
+    {
+        $format = static::randomElement(static::$universityFormats);
+
+        return $this->generator->parse($format);
+    }
 }
